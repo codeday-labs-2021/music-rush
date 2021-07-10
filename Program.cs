1bc8457a345cbd4b6aee9d6d9f1d7607cbde3d93
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-07-10 12:10:21 -0700

    starter Hello World files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace music_rush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
