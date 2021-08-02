82f839f5a539b32d93db81baeed8243248eda3e3
Luke <deandrevidal@aol.com>
2021-08-01 22:59:00 -0700

    added validation on song form

--- musicrush/Models/Song.cs
+++ musicrush/Models/Song.cs
@@ -8,13 +8,28 @@ namespace musicrush.Models
     public class Song
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(60)]
         public string Artist { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(60)]
         public string Album { get; set; }
         public int Rating { get; set; }
 
