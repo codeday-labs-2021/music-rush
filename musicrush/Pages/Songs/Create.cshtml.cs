93b64624cd9a2ac7b5c5091034835b432f11206f
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-08-06 22:26:23 -0700

    Bug Fix: add album check upon creation of a new song, eliminate foreign key constraint error

--- musicrush/Pages/Songs/Create.cshtml.cs
+++ musicrush/Pages/Songs/Create.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -26,6 +27,10 @@ namespace musicrush.Pages.Songs
         [BindProperty]
         public Song Song { get; set; }
 
+        [BindProperty]
+
+        public String AlbumTitle { get; set; }
+
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
@@ -34,6 +39,21 @@ namespace musicrush.Pages.Songs
                 return Page();
             }
 
+            if (String.IsNullOrWhiteSpace(AlbumTitle)) {
+                return RedirectToPage("/Error?AlbumTitleEmpty=true");
+            }
+
+            Album album = _context
+                            .Albums
+                            .Where(a => a.Title.ToLower().Equals(AlbumTitle.ToLower()))
+                            .FirstOrDefault();
+
+            if (album == null) {
+                return RedirectToPage("/Error?AlbumNotFound=" + HttpUtility.UrlEncode('"' + AlbumTitle + '"'));
+            }
+
+            Song.Album = album;
+            Song.AlbumId = album.ID;
             _context.Songs.Add(Song);
             await _context.SaveChangesAsync();
 
